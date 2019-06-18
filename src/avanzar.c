66cd64bf85f5bb30be0943d06a925378908f6d68
Paula <paulacaral@hotmail.com>
2019-06-18 14:15:42 -0300

    Velocity verlet en avanzar.c

--- src/avanzar.c
+++ src/avanzar.c
@@ -7,3 +7,14 @@
 #include <unistd.h>
 
 // Definicion de funciones
+
+float velocity_verlet(double* x, double* v, double* f, double h, double m)
+{
+	int i;
+	for(i = 0; i<3*N; i++)
+	{
+		x[i] = x[i] + v[i]*h + 0.5*f[t]*h*h/m; // Esto quisiera ser x(t+h)
+		v[i] = v[i] + 0.5*(f[i]+f(t+h))*h/m; // Esto quisiera ser v(t+h). COMO SE CALCULA F(T+H)? Si es en efecto como x(t+h) y v(t+h) que estamos redefiniendo, entonces se pierde la informacion de f(t) si ya reescribo f(t+h)
+	}
+	return 0.0
+}
