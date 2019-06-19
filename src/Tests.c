c81e6bd6fffcfe80c99d1401fb63363804704571
Igna11 <ignaao@gmail.com>
2019-06-18 23:20:17 -0300

    Corregidos los errores que no dejaban compilar
    Sin embargo, no printea el .txt. Corregir

--- src/Tests.c
+++ src/Tests.c
@@ -1,4 +1,9 @@
+#include "general.h"
+#include "interaccion.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 double TEST_pair_force(double r2, double* F_mod)
 {
@@ -30,25 +35,26 @@ double TEST_forces(double* dx_vector, double* F_mod, double* f, double* x, int N
 	int i, j, k;
 	
 	sprintf(filename, "FORCES_TEST.txt");
-	fp = fopen(filename, "w ")
+	fp = fopen(filename, "w ");
 	
 	//quiero setear solo 2 partículas con dos posiciones iniciales fijas y sin velocidades
 	
-	x[0] = 0.1*L; //eje x, en el 10% de L
-	x[3] = 0.9*L; //eje x, en el 90% de L
-	x[1] = x[2] = x[4] = x[5] = 0.5; //eje y, z, ambas partículas alineadas en la mitad de L
+	x[0] = 0.1*L;/*eje x en el 10% de L*/
+	x[3] = 0.9*L;/*eje x en el 90% de L*/
+	x[1] = x[2] = x[4] = x[5] = 0.5; /*eje y z ambas partículas alineadas en la mitad de L*/
 	
 	forces(dx_vector, F_mod, f, x, N, L);
 	for(i = 0; i < N - 1; i++)
 	{
-		fprintf("%i\t", i);
+		fprintf(fp,"%i\t", i);
 		for(j = i + 1; N; j++)
 		{
 			for(k = 0; k < 3; k++)
 			{
-				fprintf("%lf\n", f[3*i+k]);
+				fprintf(fp, "%lf\n", f[3*i+k]);
 			}
 		}
 	}
+	fclose(fp);
 	return 0;
 }
